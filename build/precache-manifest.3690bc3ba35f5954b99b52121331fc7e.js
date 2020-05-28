5f6db25eb5aaadf38a86291c937ed9e7c27d67f1
Garreth Dottin <garreth.dottin1@gmail.com>
2020-05-29 02:51:05 +0530

    Test

---
+++ build/precache-manifest.3690bc3ba35f5954b99b52121331fc7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f56bb5a10670cc62d5dcb6c70ed212b",
+    "url": "/TheShantam/contact-manager-react/index.html"
+  },
+  {
+    "revision": "378e4d13a9875b4ac2e9",
+    "url": "/TheShantam/contact-manager-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e5fc6360e9318bc0cb0f",
+    "url": "/TheShantam/contact-manager-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "378e4d13a9875b4ac2e9",
+    "url": "/TheShantam/contact-manager-react/static/js/2.78f56c67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TheShantam/contact-manager-react/static/js/2.78f56c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fc6360e9318bc0cb0f",
+    "url": "/TheShantam/contact-manager-react/static/js/main.0de6f82f.chunk.js"
+  },
+  {
+    "revision": "736cc28e811f85b99c66",
+    "url": "/TheShantam/contact-manager-react/static/js/runtime-main.05ab1525.js"
+  }
+]);
